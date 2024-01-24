62cb2deb4371f66e67f3e0b165c81be97e049edd
Charlie Gil <charliegil0691@gmail.com>
2024-01-24 18:59:35 -0500

    Finished!

--- comp557a1.py
+++ comp557a1.py
@@ -9,21 +9,20 @@ import taichi.math as tm
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--file", type=str, default="cube.obj")  # Add argument requires an input for
-# each added argument when called from the command line
+parser.add_argument("--file", type=str, default="cube.obj")
 parser.add_argument("--width", type=int, default=1440, help="Width of off screen framebuffer")
 parser.add_argument("--height", type=int, default=720, help="Height of off screen framebuffer")
 parser.add_argument("--px", type=int, default=10, help="Size of pixel in on screen framebuffer")
 parser.add_argument("--test", type=int, help="run a numbered unit test")
 args = parser.parse_args()
-ti.init(arch=ti.cpu)  # can also use ti.gpu -> tells program to run on cpu
-px = args.px  # Size of pixel in on screen framebuffer
-width, height = args.width // px, args.height // px  # Size of off-screen framebuffer
-pix = np.zeros((width, height, 3), dtype=np.float32)  # Pixel data with rgb values per pixel
-depth = np.zeros((width, height, 1), dtype=np.float32)  # Pixel data with z depth value per pixel
-pixti = ti.Vector.field(3, dtype=ti.f32, shape=(width, height))  # off-screen pixels
-pixels = ti.Vector.field(3, dtype=ti.f32, shape=(width * px, height * px))  # on-screen pixels
-V, _, N, T, _, TN = igl.read_obj(args.file)  # read mesh with normals, V=vertex coordinates, N=vertex
+ti.init(arch=ti.cpu)
+px = args.px
+width, height = args.width // px, args.height // px
+pix = np.zeros((width, height, 3), dtype=np.float32)
+depth = np.zeros((width, height, 1), dtype=np.float32)
+pixti = ti.Vector.field(3, dtype=ti.f32, shape=(width, height))
+pixels = ti.Vector.field(3, dtype=ti.f32, shape=(width * px, height * px))
+V, _, N, T, _, TN = igl.read_obj(args.file)
 
 
 @ti.kernel
@@ -36,37 +35,38 @@ def copy_pixels():
 
 # Generates random color, 3 values of float from 0 to 1
 def random_color():
-    r = np.random.uniform(0, 1)
-    g = np.random.uniform(0, 1)
-    b = np.random.uniform(0, 1)
-    return [r, g, b]
+    return [np.random.uniform(0, 1), np.random.uniform(0, 1), np.random.uniform(0, 1)]
 
 
+# Computes vertex normals for objects with no normal data
 def compute_normals():
-    for fidx in T:
-        points = [Vt[fidx][i] for i in range(3)]
-        e1 = np.subtract(points[1], points[0])
-        e2 = np.subtract(points[2], points[0])
-        n = np.cross(e1, e2)  # Warning is a bug in numpy
-        n = n / np.linalg.norm(n)
-        # Set normal of face and vertices to n
-        N[fidx] = n
-        T[fidx] = [fidx, fidx, fidx]
+    normals = np.ndarray((len(T), 3))
+    normal_indices = np.ndarray((len(T), 3), dtype=int)
+
+    for i in range(len(T)):
+        u, v, w = V[T[i]]
+        e1 = np.subtract(v, u)
+        e2 = np.subtract(w, u)
+        n = np.cross(e1, e2)  # compute cross product of 2 edges of triangle to get normal
+        n = n / np.linalg.norm(n)  # normalize
+        normals[i] = n  # add normal to array
+        normal_indices[i] = [i, i, i]  # update index of normals
+    return normals, normal_indices
 
 
-# Helper function for computing barycentric coordinates
-def fxy(x, y, points, t):
+# Helper function for computing barycentric coordinates, following formulae from textbook
+def fxy(x, y, points, t):  #
     x0, y0 = (points[0][i] for i in range(2))
     x1, y1 = (points[1][i] for i in range(2))
     x2, y2 = (points[2][i] for i in range(2))
 
     f = 0
 
-    if t == 1:
+    if t == 1:  # compute f01
         f = (y0 - y1) * x + (x1 - x0) * y + x0 * y1 - x1 * y0
-    if t == 12:
+    if t == 12:  # compute f12
         f = (y1 - y2) * x + (x2 - x1) * y + x1 * y2 - x2 * y1
-    if t == 20:
+    if t == 20:  # compute f20
         f = (y2 - y0) * x + (x0 - x2) * y + x2 * y0 - x0 * y2
 
     return f
@@ -97,15 +97,14 @@ while gui.running:
 
     # Compute normals if undefined
     if N.shape[0] == 0:
-        N = np.zeros(len(T))
-        compute_normals()
+        N, TN = compute_normals()
 
     Nt = (Ry @ Rx @ Rz @ N.T).T
 
-    # Q1 Compute bounding boxes
-    for fidx in T:  # for each face
+    face = 0  # Iterator
 
-        # Get coordinates of vertices of face
+    for fidx in T:
+
         vertices = [Vt[fidx][i] for i in range(3)]  # get vertex coords for face
         xs = [v[0] for v in vertices]
         ys = [v[1] for v in vertices]
@@ -139,7 +138,15 @@ while gui.running:
         fb = fxy(p1[0], p1[1], vertices, 20)
         fg = fxy(p2[0], p2[1], vertices, 1)
 
-        # Pixel walk algorithm
+        # Interpolate z value of face normal
+        normal_idx = TN[face][0]
+        normal = N[normal_idx]
+        normal_t = Nt[normal_idx]
+        normal_z = normal_t[2]
+
+        face += 1
+
+        # Pixel walk algorithm from textbook
         for y in range(yl, yh):
             for x in range(xl, xh):
 
@@ -159,33 +166,26 @@ while gui.running:
                 wz = p2[2]
                 z = alpha * uz + beta * vz + gamma * wz
 
-                #############
-                indices = TN[fidx]
-                normals = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-                bary = [alpha, beta, gamma]
-                for i in range(3):
-                    x_idx, y_idx, z_idx = (indices[i][j] for j in range(3))
-                    normals[i] = [N[x_idx][0] * bary[i], N[y_idx][1] * bary[i], N[z_idx][2] * bary[i]]
-                normal = np.add(normals[0], normals[1])
-                normal = np.add(normal, normals[2])
-                normal_z = normal[2]
-                #############
-
                 if 0 <= alpha and 0 <= beta and 0 <= gamma:
-                    if (alpha > 0 or fa * fxy(-1, -1, vertices, 12) > 0) and (beta > 0 or fb * fxy(-1, -1, vertices, 20) > 0) and (gamma > 0 or fg * fxy(-1, -1, vertices, 1) > 0):
+                    if ((alpha > 0 or fa * fxy(-1, -1, vertices, 12) > 0) and
+                            (beta > 0 or fb * fxy(-1, -1, vertices, 20) > 0) and
+                            (gamma > 0 or fg * fxy(-1, -1, vertices, 1) > 0)):
 
                         # Q2 Draw triangles (no depth)
                         if args.test == 2:
                             pix[x][y] = [alpha, beta, gamma]
                             continue
 
-                        if z > depth[x][y]:
+                        # Check for depth
+                        if z > depth[x][y]:  # If z is closer to us than previous, update and draw
                             depth[x][y] = z
 
+                            # Q3 Draw object while considering depth
                             if args.test == 3:
                                 pix[x][y] = [alpha, beta, gamma]
                                 continue
 
+                            # Q4 Use interpolated z value of normal as greyscale
                             if normal_z > 0:
                                 r = g = b = normal_z
                             else:
